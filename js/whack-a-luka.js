1f2547664ef47fe533cb27a9f771a2473ba0fa66
xp3d1 <62486015+xp3d1@users.noreply.github.com>
2020-07-14 18:07:38 +0100

    eyy

--- js/whack-a-luka.js
+++ js/whack-a-luka.js
@@ -5,13 +5,18 @@ let score = document.querySelector("#score");
 let result = 0;
 let hitPosition = 0;
 var hit = false;
+var randomPosition = gridItems[Math.floor(Math.random() * 9)];
+setInterval(function(){
+    randomSquare()
+}, 1000);
 
 function randomSquare(){
     gridItems.forEach(className => {
         className.classList.remove("mole");
+        className.classList.remove("moleHIT");
     });
 
-    let randomPosition = gridItems[Math.floor(Math.random() * 9)];
+    randomPosition = gridItems[Math.floor(Math.random() * 9)];
     randomPosition.classList.add("mole");
 
     hitPosition = randomPosition.id;
@@ -23,11 +28,9 @@ gridItems.forEach(item => {
         if(item.id === hitPosition && !hit){
             result += 1;
             score.textContent = result;
+            randomPosition.classList.remove("mole");
+            randomPosition.classList.add("moleHIT");
             hit = true;
         }
     })
 });
-
-setInterval(function(){
-    randomSquare()
-}, 1000);
